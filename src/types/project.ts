a616e8bc3d728366cb10a7f13d25738294fb6593
gabriel.algo <85095069+GGManera@users.noreply.github.com>
2025-10-17 13:44:37 -0300

    Reverted all changes back to version 4e0bbb0d8e0dc3ab235ed9c8febd16c28cd9eeb3

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,11 +1,7 @@
-export interface ProjectMetadataItem {
-  type: string;
+export interface MetadataItem {
+  title: string;
   value: string;
+  type?: 'url' | 'x-url' | 'asset-id' | 'address' | 'text' | 'project-name' | 'project-description' | 'whitelisted-editors' | 'is-creator-added' | 'added-by-address' | 'is-community-notes' | 'tags' | 'is-claimed' | 'project-wallet'; // Added 'project-wallet'
 }
 
-export interface ProjectDetails {
-  projectId: string;
-  description: string;
-  projectMetadata: ProjectMetadataItem[];
-  // Add other necessary fields here
-}
+export type ProjectMetadata = MetadataItem[];

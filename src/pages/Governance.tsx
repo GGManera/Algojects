7ac7fde1fbf933757aecd96af7bff0cea06f14f5
gabriel.algo <gbrgmanera@gmail.com>
2025-11-06 16:37:05 -0300

    [dyad] Reduzindo o espaço entre as barras do gráfico de classificação e forçando a exibição de todos os rótulos do eixo X. - wrote 1 file(s)

--- src/pages/Governance.tsx
+++ src/pages/Governance.tsx
@@ -447,9 +447,9 @@ const GovernancePage = () => {
                                     {/* RATING: Bar Chart only */}
                                     {qStats.type === 'rating' && (
                                         <ResponsiveContainer width="100%" height={200}>
-                                            <BarChart data={qStats.data} margin={{ top: 5, right: 0, left: -5, bottom: 5 }} barCategoryGap={10}> {/* ADDED barCategoryGap={10} */}
+                                            <BarChart data={qStats.data} margin={{ top: 5, right: 0, left: -5, bottom: 5 }} barCategoryGap={1}> {/* Adjusted barCategoryGap to 1 */}
                                                 {/* Use CustomStarTick for yellow stars */}
-                                                <XAxis dataKey="name" stroke="hsl(var(--muted-foreground))" tick={<CustomStarTick />} />
+                                                <XAxis dataKey="name" stroke="hsl(var(--muted-foreground))" tick={<CustomStarTick />} interval={0} /> {/* Added interval={0} */}
                                                 <YAxis 
                                                     stroke="hsl(var(--muted-foreground))" 
                                                     allowDecimals={false} 
